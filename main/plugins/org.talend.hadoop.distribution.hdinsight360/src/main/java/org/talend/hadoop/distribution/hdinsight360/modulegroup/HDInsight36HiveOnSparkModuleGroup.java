704e7b7fd313ecef3efb260bb41d4e9ce3bf91a6
slemoing <stefan.lemoing@gmail.com>
2017-04-24 18:33:19 +0200

    feat(TBD-4443): fix hive depencies issue

--- main/plugins/org.talend.hadoop.distribution.hdinsight360/src/main/java/org/talend/hadoop/distribution/hdinsight360/modulegroup/HDInsight36HiveOnSparkModuleGroup.java
+++ main/plugins/org.talend.hadoop.distribution.hdinsight360/src/main/java/org/talend/hadoop/distribution/hdinsight360/modulegroup/HDInsight36HiveOnSparkModuleGroup.java
@@ -16,24 +16,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.talend.hadoop.distribution.DistributionModuleGroup;
-import org.talend.hadoop.distribution.condition.BasicExpression;
-import org.talend.hadoop.distribution.condition.ComponentCondition;
-import org.talend.hadoop.distribution.condition.EqualityOperator;
-import org.talend.hadoop.distribution.condition.SimpleComponentCondition;
-import org.talend.hadoop.distribution.constants.SparkBatchConstant;
 import org.talend.hadoop.distribution.hdinsight360.HDInsight36Constant;
 
 public class HDInsight36HiveOnSparkModuleGroup {
 
-    private final static ComponentCondition condition = new SimpleComponentCondition(new BasicExpression(
-            SparkBatchConstant.SPARK_LOCAL_MODE_PARAMETER, EqualityOperator.EQ, "false")); //$NON-NLS-1$
-
     public static Set<DistributionModuleGroup> getModuleGroups() {
         Set<DistributionModuleGroup> hs = new HashSet<>();
-        hs.add(new DistributionModuleGroup(HDInsight36Constant.SPARK_HIVE_MRREQUIRED_MODULE_GROUP.getModuleName(), false,
-                condition));
-        hs.add(new DistributionModuleGroup(HDInsight36Constant.SPARK_SQL_MRREQUIRED_MODULE_GROUP.getModuleName(), false,
-                condition));
+        hs.add(new DistributionModuleGroup(HDInsight36Constant.SPARK_HIVE_MRREQUIRED_MODULE_GROUP.getModuleName(), false, null));
+        hs.add(new DistributionModuleGroup(HDInsight36Constant.SPARK_SQL_MRREQUIRED_MODULE_GROUP.getModuleName(), false, null));
         return hs;
+
     }
 }
