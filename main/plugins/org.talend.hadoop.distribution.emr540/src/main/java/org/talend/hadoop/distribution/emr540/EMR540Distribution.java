8f0e40ae06e371f352720db836e2c811877ab329
Remy Dubois <rdubois@talend.com>
2017-04-24 11:20:59 +0200

    Few fixes in EMR 5.4.0 + Upgrade to Spark 2.1

--- main/plugins/org.talend.hadoop.distribution.emr540/src/main/java/org/talend/hadoop/distribution/emr540/EMR540Distribution.java
+++ main/plugins/org.talend.hadoop.distribution.emr540/src/main/java/org/talend/hadoop/distribution/emr540/EMR540Distribution.java
@@ -73,13 +73,13 @@ public class EMR540Distribution extends AbstractDistribution implements HBaseCom
 
     public static final String VERSION = "EMR_5_4_0"; //$NON-NLS-1$
 
-    public static final String VERSION_DISPLAY = "EMR 5.4.0 (Apache 2.7.2)"; //$NON-NLS-1$
+    public static final String VERSION_DISPLAY = "EMR 5.4.0 (Apache 2.7.3)"; //$NON-NLS-1$
 
     public static final String PIG_EMR540_DISPLAY = "EMR 5.4.0 (Pig 0.16.0)";//$NON-NLS-1$
 
     public static final String SQOOP_EMR540_DISPLAY = "EMR 5.4.0 (Sqoop 1.4.6)"; //$NON-NLS-1$
 
-    public static final String HIVE_EMR540_DISPLAY = "EMR 5.4.0 (Hive 2.1.0)"; //$NON-NLS-1$
+    public static final String HIVE_EMR540_DISPLAY = "EMR 5.4.0 (Hive 2.1.1)"; //$NON-NLS-1$
 
     private final static String YARN_APPLICATION_CLASSPATH = "$HADOOP_CONF_DIR,$HADOOP_COMMON_HOME/*,$HADOOP_COMMON_HOME/lib/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$HADOOP_YARN_HOME/*,$HADOOP_YARN_HOME/lib/*,/usr/lib/hadoop-lzo/lib/*,/usr/share/aws/emr/emrfs/conf, /usr/share/aws/emr/emrfs/lib/*,/usr/share/aws/emr/emrfs/auxlib/*,/usr/share/aws/emr/lib/*,/usr/share/aws/emr/ddb/lib/emr-ddb-hadoop.jar, /usr/share/aws/emr/goodies/lib/emr-hadoop-goodies.jar,/usr/share/aws/emr/kinesis/lib/emr-kinesis-hadoop.jar,/usr/lib/spark/yarn/lib/datanucleus-api-jdo.jar,/usr/lib/spark/yarn/lib/datanucleus-core.jar,/usr/lib/spark/yarn/lib/datanucleus-rdbms.jar,/usr/share/aws/emr/cloudwatch-sink/lib/*"; //$NON-NLS-1$
 
@@ -339,7 +339,7 @@ public class EMR540Distribution extends AbstractDistribution implements HBaseCom
     @Override
     public Set<ESparkVersion> getSparkVersions() {
         Set<ESparkVersion> version = new HashSet<>();
-        version.add(ESparkVersion.SPARK_2_0);
+        version.add(ESparkVersion.SPARK_2_1);
         return version;
     }
 
