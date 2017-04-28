b3de5e7f7da55892771982fdc7b533f9fd208b55
Remy Dubois <rdubois@talend.com>
2017-04-28 16:31:04 +0200

    Move from EMR 5.4 to 5.5

---
+++ main/plugins/org.talend.hadoop.distribution.emr550/src/main/java/org/talend/hadoop/distribution/emr550/modulegroup/node/sparkbatch/EMR550GraphFramesNodeModuleGroup.java
@@ -0,0 +1,21 @@
+// ============================================================================
+package org.talend.hadoop.distribution.emr550.modulegroup.node.sparkbatch;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.condition.common.SparkBatchLinkedNodeCondition;
+
+public class EMR550GraphFramesNodeModuleGroup {
+
+    public static final String GRAPHFRAMES_GROUP_NAME = "GRAPHFRAMES-LIB-EMR_5_5_0_LATEST"; //$NON-NLS-1$
+
+    public static Set<DistributionModuleGroup> getModuleGroups(String distribution, String version) {
+        Set<DistributionModuleGroup> hs = new HashSet<>();
+        DistributionModuleGroup dmg = new DistributionModuleGroup(GRAPHFRAMES_GROUP_NAME, true,
+                new SparkBatchLinkedNodeCondition(distribution, version).getCondition());
+        hs.add(dmg);
+        return hs;
+    }
+}
