6f413fd490c2f232b14f4fd475f48afbecaeebfa
slemoing <slemoing@talend.com>
2018-02-14 14:25:36 +0100

    feat(TBD-5648): navigator clean code

--- main/components_lib/talend-lineage-api/src/test/java/org/talend/lineage/cloudera/MyIterableTestCDH513.java
+++ main/components_lib/talend-lineage-api/src/test/java/org/talend/lineage/cloudera/MyIterableTestCDH513.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.lineage.cloudera;
 
 import java.util.List;
@@ -10,14 +22,12 @@ public class MyIterableTestCDH513<T> implements Iterable<T> {
     private List<T> list;
 
     public MyIterableTestCDH513(T [] t) {
-
         list = Arrays.asList(t);
         Collections.reverse(list);
     }
 
     @Override
     public Iterator<T> iterator() {
-
         return list.iterator();
     }
 }
