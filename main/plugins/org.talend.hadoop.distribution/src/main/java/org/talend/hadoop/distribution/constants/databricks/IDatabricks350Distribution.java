54dd965fa0ae41ed03ae6006fd1744b12d615cef
sponomarova <sponomarova@talend.com>
2019-10-01 16:53:12 +0300

    fix(TBD-9299): whiteList Snowflake components for Databricks 5.4

---
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/constants/databricks/IDatabricks350Distribution.java
@@ -0,0 +1,23 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.hadoop.distribution.constants.databricks;
+
+@SuppressWarnings("nls")
+public interface IDatabricks350Distribution extends IDatabricksDistribution {
+
+	 public final static String VERSION = "Databricks_3_5";
+
+	 public static final String VERSION_DISPLAY = "3.5 LTS (includes Apache Spark 2.2.1, Scala 2.11)";
+}
+
+
---
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/constants/databricks/IDatabricks540Distribution.java
@@ -0,0 +1,23 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.hadoop.distribution.constants.databricks;
+
+@SuppressWarnings("nls")
+public interface IDatabricks540Distribution extends IDatabricksDistribution {
+
+    public final static String VERSION = "Databricks_5_4";
+
+    public static final String VERSION_DISPLAY = "5.4 (includes Apache Spark 2.4.3, Scala 2.11)";
+}
+
+
