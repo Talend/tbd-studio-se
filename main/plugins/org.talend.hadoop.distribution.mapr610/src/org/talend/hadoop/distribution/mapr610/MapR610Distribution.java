3ba999d17a88fbe06ca48ae1a93cf89ea204135a
Andrii Medvedenko <amedvedenko@talend.com>
2019-10-04 13:50:40 +0300

    TBD-9230 and TBD-9229: switching mapr to use new s3 AWS API instead of old one

--- main/plugins/org.talend.hadoop.distribution.mapr610/src/org/talend/hadoop/distribution/mapr610/MapR610Distribution.java
+++ main/plugins/org.talend.hadoop.distribution.mapr610/src/org/talend/hadoop/distribution/mapr610/MapR610Distribution.java
@@ -492,4 +492,9 @@ public class MapR610Distribution extends AbstractMapRDistribution implements HDF
     public boolean doSupportStoreAsParquet() {
         return false;
     }
+
+    @Override
+    public boolean useOldAWSAPI() {
+        return false;
+    }
 }
