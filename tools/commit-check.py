dd9a01476e22d2a27f80d382f7b854a20c46754c
Jean-Philippe HAUTIN <jphautin@talend.com>
2019-11-13 15:56:07 +0100

    feat: print value name and its value

--- tools/commit-check.py
+++ tools/commit-check.py
@@ -1,6 +1,21 @@
 import os
+import re
 
-semantic_commits_regex = "(?:chore|docs|feat|fix|refactor|style|test)[\(\[](?:TBD|TDI|TUP|DEVOPS)-(?:\d{4})[)\]]:.*"
+team_tickets = "TBD|TDI|TUP|DEVOPS"
+semantics = "chore|docs|feat|fix|refactor|style|test"
+
+tbd_commit_regex = "(?:%s)[\(\[\:](?:%s)-(?:\d{4})([)\]]:|:).**" % (semantics, team_tickets)
+
+# feat(TBD-9011): tata
+# chore[TBD-9011]: titi
+# refactor:TBD-9011: toto
+
+COLOR_RED = '\033[1;31m'
+END_COLOR = '\033[1;m'
+
+
+def red(text):
+    return '%s%s%s' % (COLOR_RED, str(text), END_COLOR)
 
 
 def check_commit_message():
@@ -20,6 +35,13 @@ def check_commit_message():
         print(os.environ['TAG_TIMESTAMP'])
         print(os.environ['TAG_UNIXTIME'])
         print(os.environ['TAG_UNIXTIME'])
+    result = re.search(tbd_commit_regex, os.environ['CHANGE_TITLE'])
+    if result is None:
+        print("ERROR : the message does not follow convention. %s" % red(os.environ['CHANGE_TITLE']))
+        print("")
+    (start, end) = result.span()
+    if start != 0:
+        print("ERROR : unexpected prefix %s" % red(result[0:start]))
 
 
 if __name__ == '__main__':
