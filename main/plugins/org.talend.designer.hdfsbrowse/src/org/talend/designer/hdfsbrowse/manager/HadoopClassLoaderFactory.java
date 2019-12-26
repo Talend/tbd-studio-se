e6804ed4c5f84e75342baf0c33e23c710cfc9b0c
Chao MENG <cmeng@talend.com>
2019-12-26 18:41:49 +0800

    feat(TUP-25226): Make the hadoop conf jar path a contextualizable parameter in the Hadoop conf wizard
    https://jira.talendforge.org/browse/TUP-25226

--- main/plugins/org.talend.designer.hdfsbrowse/src/org/talend/designer/hdfsbrowse/manager/HadoopClassLoaderFactory.java
+++ main/plugins/org.talend.designer.hdfsbrowse/src/org/talend/designer/hdfsbrowse/manager/HadoopClassLoaderFactory.java
@@ -71,12 +71,14 @@ public class HadoopClassLoaderFactory {
                 if (confJarBean != null) {
                     Consumer<DynamicClassLoader> afterLoad = null;
                     String[] addingJars = null;
-                    String overrideCustomConfPath = confJarBean.getOriginalOverrideCustomConfPath();
-                    if (StringUtils.isBlank(overrideCustomConfPath) || !new File(overrideCustomConfPath).exists()) {
-                        ExceptionHandler.process(
-                                new Exception("Set Hadoop configuration JAR path is invalid: " + overrideCustomConfPath));
-                    } else {
-                        afterLoad = (t) -> t.addLibrary(overrideCustomConfPath);
+                    if (confJarBean.isOverrideCustomConf()) {
+                        String overrideCustomConfPath = confJarBean.getOriginalOverrideCustomConfPath();
+                        if (StringUtils.isBlank(overrideCustomConfPath) || !new File(overrideCustomConfPath).exists()) {
+                            ExceptionHandler.process(
+                                    new Exception("Set Hadoop configuration JAR path is invalid: " + overrideCustomConfPath));
+                        } else {
+                            afterLoad = (t) -> t.addLibrary(overrideCustomConfPath);
+                        }
                     }
                     String customConfsJarName = confJarBean.getCustomConfJarName();
                     if (customConfsJarName != null) {
