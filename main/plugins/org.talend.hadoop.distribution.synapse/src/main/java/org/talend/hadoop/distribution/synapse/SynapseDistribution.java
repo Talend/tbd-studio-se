498bdf6f791e218a1dde9eac8d8c8bf1fac90717
Svitlana Ponomarova <sponomarova@talend.com>
2021-06-24 21:02:41 +0300

    feat(TBD-12339): Synapse TP

--- main/plugins/org.talend.hadoop.distribution.synapse/src/main/java/org/talend/hadoop/distribution/synapse/SynapseDistribution.java
+++ main/plugins/org.talend.hadoop.distribution.synapse/src/main/java/org/talend/hadoop/distribution/synapse/SynapseDistribution.java
@@ -11,7 +11,7 @@
 //
 // ============================================================================
 
-package org.talend.hadoop.distribution.emr62x;
+package org.talend.hadoop.distribution.synapse;
 
 import java.util.Arrays;
 import java.util.HashMap;
