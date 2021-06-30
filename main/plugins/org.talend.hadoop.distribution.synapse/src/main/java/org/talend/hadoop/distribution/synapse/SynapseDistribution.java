b7f3a332a7af3b86893ec612e91c43d296569279
Svitlana Ponomarova <sponomarova@talend.com>
2021-06-30 13:52:20 +0300

    feat(TBD-12139): Synapse TP

--- main/plugins/org.talend.hadoop.distribution.synapse/src/main/java/org/talend/hadoop/distribution/synapse/SynapseDistribution.java
+++ main/plugins/org.talend.hadoop.distribution.synapse/src/main/java/org/talend/hadoop/distribution/synapse/SynapseDistribution.java
@@ -45,7 +45,7 @@ public class SynapseDistribution extends AbstractDistribution implements ISynaps
 
     public static final String VERSION_DISPLAY = "Azure Synapse Runtime for Apache Spark 3.0"; //$NON-NLS-1$
     
-    public final static ESparkVersion SPARK_VERSION = ESparkVersion.SPARK_3_1;
+    public final static ESparkVersion SPARK_VERSION = ESparkVersion.SPARK_3_0;
 
 	private final static String YARN_APPLICATION_CLASSPATH = "$HADOOP_CONF_DIR,$HADOOP_COMMON_HOME/*,$HADOOP_COMMON_HOME/lib/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$YARN_HOME/*,$YARN_HOME/lib/*,$HADOOP_YARN_HOME/*,$HADOOP_YARN_HOME/lib/*,$HADOOP_COMMON_HOME/share/hadoop/common/*,$HADOOP_COMMON_HOME/share/hadoop/common/lib/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/lib/*,$HADOOP_YARN_HOME/share/hadoop/yarn/*,$HADOOP_YARN_HOME/share/hadoop/yarn/lib/*" ; //$NON-NLS-1$
 	
