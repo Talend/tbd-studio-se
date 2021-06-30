f9d9ebc6ccadb50557e6d8d7011c93930660a37e
Svitlana Ponomarova <sponomarova@talend.com>
2021-06-30 10:32:03 +0300

    feat(TBD-12139): Synapse TP

--- main/plugins/org.talend.hadoop.distribution.synapse/src/main/java/org/talend/hadoop/distribution/synapse/SynapseDistribution.java
+++ main/plugins/org.talend.hadoop.distribution.synapse/src/main/java/org/talend/hadoop/distribution/synapse/SynapseDistribution.java
@@ -13,10 +13,14 @@
 
 package org.talend.hadoop.distribution.synapse;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.Collections;
 
 import org.talend.hadoop.distribution.AbstractDistribution;
 import org.talend.hadoop.distribution.ComponentType;
@@ -24,34 +28,38 @@ import org.talend.hadoop.distribution.DistributionModuleGroup;
 import org.talend.hadoop.distribution.EHadoopVersion;
 import org.talend.hadoop.distribution.ESparkVersion;
 import org.talend.hadoop.distribution.NodeComponentTypeBean;
+import org.talend.hadoop.distribution.component.HDFSComponent;
 import org.talend.hadoop.distribution.component.MRComponent;
 import org.talend.hadoop.distribution.component.SparkBatchComponent;
 import org.talend.hadoop.distribution.component.SparkStreamingComponent;
 import org.talend.hadoop.distribution.condition.ComponentCondition;
-import org.talend.hadoop.distribution.constants.SparkBatchConstant;
-import org.talend.hadoop.distribution.constants.SparkStreamingConstant;
+import org.talend.hadoop.distribution.constants.ModuleGroupName;
+import org.talend.hadoop.distribution.spark.SparkClassPathUtils;
 import org.talend.hadoop.distribution.constants.synapse.ISynapseDistribution;
 
-public class SynapseDistribution extends AbstractDistribution  implements SparkBatchComponent, SparkStreamingComponent,
-	ISynapseDistribution, MRComponent {
+@SuppressWarnings("nls")
+public class SynapseDistribution extends AbstractDistribution implements ISynapseDistribution, HDFSComponent, MRComponent,
+        SparkBatchComponent, SparkStreamingComponent {
 
-	public static final String VERSION = "SYNAPSE"; //$NON-NLS-1$
+    public static final String VERSION = "SYNAPSE"; //$NON-NLS-1$
 
     public static final String VERSION_DISPLAY = "Azure Synapse Runtime for Apache Spark 3.0"; //$NON-NLS-1$
 
-    private final static String YARN_APPLICATION_CLASSPATH = "$HADOOP_CONF_DIR,$HADOOP_COMMON_HOME/*,$HADOOP_COMMON_HOME/lib/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$YARN_HOME/*,$YARN_HOME/lib/*,$HADOOP_YARN_HOME/*,$HADOOP_YARN_HOME/lib/*,$HADOOP_COMMON_HOME/share/hadoop/common/*,$HADOOP_COMMON_HOME/share/hadoop/common/lib/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/lib/*,$HADOOP_YARN_HOME/share/hadoop/yarn/*,$HADOOP_YARN_HOME/share/hadoop/yarn/lib/*" ; //$NON-NLS-1$
+	private final static String YARN_APPLICATION_CLASSPATH = "$HADOOP_CONF_DIR,$HADOOP_COMMON_HOME/*,$HADOOP_COMMON_HOME/lib/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$YARN_HOME/*,$YARN_HOME/lib/*,$HADOOP_YARN_HOME/*,$HADOOP_YARN_HOME/lib/*,$HADOOP_COMMON_HOME/share/hadoop/common/*,$HADOOP_COMMON_HOME/share/hadoop/common/lib/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/lib/*,$HADOOP_YARN_HOME/share/hadoop/yarn/*,$HADOOP_YARN_HOME/share/hadoop/yarn/lib/*" ; //$NON-NLS-1$
+	private final static String CLASSPATH_SEPARATOR = ",";
+	
+	public final static String DEFAULT_LIB_ROOT = "/usr/lib";
+	
+    protected Map<ComponentType, Set<DistributionModuleGroup>> moduleGroups;
 
-    protected Map<ComponentType, String> customVersionDisplayNames;
-    
-    private static Map<ComponentType, Set<DistributionModuleGroup>> moduleGroups;
+	protected Map<NodeComponentTypeBean, Set<DistributionModuleGroup>> nodeModuleGroups;
 
-    private static Map<NodeComponentTypeBean, Set<DistributionModuleGroup>> nodeModuleGroups;
+	protected Map<ComponentType, ComponentCondition> displayConditions;
 
-    private static Map<ComponentType, ComponentCondition> displayConditions = new HashMap<>();
+	protected Map<ComponentType, String> customVersionDisplayNames;
 
-    public SynapseDistribution() {
-
-    	displayConditions = buildDisplayConditions();
+	public SynapseDistribution() {
+		displayConditions = buildDisplayConditions();
         customVersionDisplayNames = buildCustomVersionDisplayNames();
         moduleGroups = buildModuleGroups();
         nodeModuleGroups = buildNodeModuleGroups(getDistribution(), getVersion());        
@@ -64,119 +72,112 @@ public class SynapseDistribution extends AbstractDistribution  implements SparkB
 	protected Map<ComponentType, String> buildCustomVersionDisplayNames() {
         return new HashMap<>();
     }
+	
+	@Override
+	public String getDistribution() {
+		return DISTRIBUTION_NAME;
+	}
 
-    @Override
-    public boolean doSupportOldImportMode() {
-        return false;
+	@Override
+	public String getDistributionName() {
+		return DISTRIBUTION_DISPLAY_NAME;
+	}
+
+	@Override
+	public String getVersion() {
+		return VERSION;
+	}
+	
+	@Override
+	public boolean isSynapseDistribution() {
+		return true;
+	}
+
+	@Override
+	public String getVersionName(ComponentType componentType) {
+		return VERSION_DISPLAY;
+	}
+
+	@Override
+	public EHadoopVersion getHadoopVersion() {
+		return EHadoopVersion.HADOOP_3;
+	}
+
+	@Override
+	public boolean doSupportKerberos() {
+		return false;
+	}
+
+	@Override
+	public Set<DistributionModuleGroup> getModuleGroups(
+			ComponentType componentType) {
+		return moduleGroups.get(componentType);
+	}
+
+	@Override
+	public Set<DistributionModuleGroup> getModuleGroups(
+			ComponentType componentType, String componentName) {
+		return nodeModuleGroups.get(new NodeComponentTypeBean(componentType,
+				componentName));
+	}
+
+	@Override
+	public boolean doSupportUseDatanodeHostname() {
+		return true;
+	}
+	
+	@Override
+	public boolean doSupportImpersonation() {
+		return false;
+	}
+	
+	@Override
+	public boolean doSupportCrossPlatformSubmission() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportSequenceFileShortType() {
+		return true;
+	}
+
+	@Override
+	public String getYarnApplicationClasspath() {
+		return YARN_APPLICATION_CLASSPATH;
+	}
+	
+	@Override
+	public String generateSparkJarsPaths(List<String> commandLineJarsPaths, boolean isLightWeight) {
+        return isLightWeight ? "" : generateSparkJarsPaths(commandLineJarsPaths);
     }
 
-    @Override
-    public boolean doRequireElasticsearchSparkPatch() {
-        return false;
+	@Override
+	public String generateSparkJarsPaths(List<String> commandLineJarsPaths) {
+        return SparkClassPathUtils.generateSparkJarsPaths(commandLineJarsPaths, ModuleGroupName.SPARK_BATCH.get(this.getVersion()));
     }
 
-    @Override
-    public String getDistribution() {
-        return DISTRIBUTION_NAME;
-    }
-
-    @Override
-    public String getDistributionName() {
-        return DISTRIBUTION_DISPLAY_NAME;
-    }
-
-    @Override
-    public String getVersion() {
-        return VERSION;
-    }
-
-    @Override
-    public String getVersionName(ComponentType componentType) {
-        return VERSION_DISPLAY;
-    }
-
-    @Override
-    public EHadoopVersion getHadoopVersion() {
-        return EHadoopVersion.HADOOP_3;
-    }
-
-    @Override
-    public boolean doSupportKerberos() {
-        return false;
-    }
-
-    @Override
-    public Set<DistributionModuleGroup> getModuleGroups(ComponentType componentType) {
-        return moduleGroups.get(componentType);
-    }
-
-    @Override
-    public Set<DistributionModuleGroup> getModuleGroups(ComponentType componentType, String componentName) {
-        return nodeModuleGroups.get(new NodeComponentTypeBean(componentType, componentName));
-    }
-
-    @Override
-    public boolean isExecutedThroughWebHCat() {
-        return true;
-    }
-
-    @Override
-    public boolean doSupportUseDatanodeHostname() {
-        return false;
-    }
-
-    @Override
-    public boolean doSupportCrossPlatformSubmission() {
-        return false;
-    }
-
-    @Override
-    public String getYarnApplicationClasspath() {
-        return YARN_APPLICATION_CLASSPATH;
-    }
-
-    @Override
-    public boolean doSupportImpersonation() {
-        return false;
-    }
-
-    @Override
-    public boolean doSupportEmbeddedMode() {
-        return false;
-    }
-
-    @Override
-    public boolean doSupportStandaloneMode() {
-        return false;
-    }
-
-    @Override
-    public boolean doSupportDynamicMemoryAllocation() {
-        return false;
-    }
-
-    @Override
-    public boolean isExecutedThroughLivy() {
-        return true;
-    }
-
-    @Override
-    public boolean doSupportCheckpointing() {
-        return false;
-    }
-
-    @Override
-    public ComponentCondition getDisplayCondition(ComponentType componentType) {
-        return displayConditions.get(componentType);
-    }
-
-    @Override
+	
+	@Override
     public Set<ESparkVersion> getSparkVersions() {
-        Set<ESparkVersion> version = new HashSet<>();
-        version.add(ESparkVersion.SPARK_3_0);
-        return version;
+		return Collections.singleton(ESparkVersion.SPARK_3_0);
     }
 
+    @Override
+    public boolean isExecutedThroughSparkJobServer() {
+        return false;
+    }
+
+	@Override
+	public ComponentCondition getDisplayCondition(ComponentType componentType) {
+		return displayConditions.get(componentType);
+	}
+
+	@Override
+	public boolean doSupportOldImportMode() {
+		return false;
+	}
+
+	
     @Override
     public boolean doSupportSparkStandaloneMode() {
         return false;
@@ -186,39 +187,46 @@ public class SynapseDistribution extends AbstractDistribution  implements SparkB
     public boolean doSupportSparkYarnClientMode() {
         return false;
     }
-
+    
     @Override
-    public boolean doSupportBackpressure() {
-        return false;
-    }
-
+    public boolean doSupportSparkYarnClusterMode() {
+    	return true;
+    };
+    
     @Override
-    public boolean doSupportHDFSEncryption() {
-        return false;
-    }
-
-    @Override
-    public boolean doSupportOozie() {
-        return false;
-    }
-
-    @Override
-    public boolean isCloudDistribution() {
+    public boolean doSupportDynamicMemoryAllocation() {
         return true;
     }
 
     @Override
-    public boolean useCloudLauncher() {
+    public boolean doSupportCheckpointing() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportBackpressure() {
         return true;
     }
 
     @Override
     public boolean doSupportAzureBlobStorage() {
+        return true;
+    }
+
+    @Override
+    public boolean useOldAWSAPI() {
         return false;
     }
 
     @Override
-    public boolean doSupportAzureDataLakeStorage() {
-        return true;
+    public short orderingWeight() {
+        return 10;
     }
+
+    @Override
+    public boolean doSupportLightWeight() {
+        return false;
+    }
+    
+   
 }
